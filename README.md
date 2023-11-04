233e7721f0965ed27b138bd390a586e65d7b745b
Vinicius Fiedler <73503780+viniFiedler@users.noreply.github.com>
2023-11-03 21:45:11 -0300

    Mudanças do bot - implementação de mandar aúdios - Atualizei o README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# TelegramBot
-Simple Telegram Bot using Python
+# Telegram JOJO Bot
+
+![GitHub repo size](https://img.shields.io/github/repo-size/viniFiedler/TelegramBot?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/viniFiedler/TelegramBot?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/viniFiedler/TelegramBot?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/viniFiedler/TelegramBot?style=for-the-badge)
+![Bitbucket open pull requests](https://img.shields.io/bitbucket/pr-raw/viniFiedler/TelegramBot?style=for-the-badge)
+
+> Um bot simples do Telegram
+
+## 💻 Pré-requisitos
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- Você possui a pyTelegramBotAPI
+  - Se não tiver digite no terminal ```pip install pyTelegramBotAPI```
+- Você tem uma máquina `Windows` (não testei no Unix mas não é para dar problema).
+- Você possuiu uma `chave API do telegram`.
+  - Para adquirir uma chave API procure por BotFather na barra de pesquisa do Telegram (Confirme o selo de verificado)
+  - Digite ``` /newbot ```
+  - Siga as instruções
+
+## ☕ Usando JOJO Bot
+
+Para usar o JOJO Bot, siga estas etapas:
+
+- Vá para o arquivo [api](api.py) e coloque sua chave API do telegram
+- Execute o código do arquivo [bot](bot.py)
+- Vá para o link do bot que o FatherBot passou
+- Enjoy
+
+## 📝 Licença
+
+Esse projeto está sob licença. Veja o arquivo [LICENÇA](LICENSE.md) para mais detalhes.
--- bot.py
+++ bot.py
@@ -1,2 +1,78 @@
 import telebot
+import api
+
+CHAVE_API = api.api_key
+
+bot = telebot.TeleBot(CHAVE_API)
+
+previous_command = {}
+
+@bot.message_handler(commands=["yareyare"]) 
+def opcao1(mensagem):
+    bot.send_message(mensagem,"Yare yare, pare de encher o meu saco")
+
+@bot.message_handler(commands=["Jotaro"]) 
+def opcao2(mensagem):
+    bot.reply_to(mensagem,"""Sim, meu nome é Jotaro Kujo
+                 /Zawarudo
+                 /yare""")
+    previous_command[mensagem.chat.id] = "/Jotaro"
+
+@bot.message_handler(commands=["Dio"]) 
+def opcao3(mensagem):
+    bot.send_message(mensagem.chat.id,"""Oh, você está se aproximando de mim? Em vez de fugir, você está vindo diretamente até mim?
+                     /Zawarudo""")
+    previous_command[mensagem.chat.id] = "/Dio"
+
+@bot.message_handler(commands=['Zawarudo'])
+def the_word_command(mensagem):
+    user_id = mensagem.chat.id
+
+    # Check the user's previous command
+    if user_id in previous_command:
+        previous_cmd = previous_command[user_id]
+        if previous_cmd == "/Jotaro":
+            # If the previous command was /jotaro, send audio
+            bot.send_audio(chat_id=mensagem.chat.id, audio=open('resources/zauwarudo_jotaro.m4a', 'rb'))
+        elif previous_cmd == "/Dio":
+            # If the previous command was /Dio, send text
+            bot.send_audio(chat_id=mensagem.chat.id, audio=open('resources/zauwarudo_dio.m4a', 'rb'))
+    else:
+        bot.send_message(user_id, """Selecione alguém antes
+                         /Dio
+                         /Jotaro""")
+
+@bot.message_handler(commands=['yare'])
+def yareyare_command(mensagem):
+    user_id = mensagem.chat.id
+    # Check the user's previous command
+    if user_id in previous_command:
+        previous_cmd = previous_command[user_id]
+        if previous_cmd == "/Jotaro":
+            # If the previous command was /jotaro, send audio
+            bot.send_audio(chat_id=mensagem.chat.id, audio=open('resources/yareyare_jotaro_Kujo.m4a', 'rb'))
+    else:
+        bot.send_message(user_id, """Você selecionou alguém que não possui esse comando""")
+
+@bot.message_handler(commands=['start'])
+def start_command(mensagem):
+    texto = """
+    "Seja bem vindo, escolha um personagem
+    /Jotaro
+    /Dio"""
+    bot.reply_to(mensagem, texto)
+    
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(func=verificar)  
+def responder(mensagem):
+    texto = """
+    "Yare Yare, não entendi o que você queria dizer, escolha uma das opções por favor
+    /Jotaro
+    /Dio"""
+    bot.reply_to(mensagem, texto)
+    
+bot.polling()
+
 
